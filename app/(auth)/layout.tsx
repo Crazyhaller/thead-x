e8a6aa45ec0b0e0e2d000cbc8426cbce7be2dd47
crazyhaller <suvigyamadrid@gmail.com>
2023-08-04 21:33:19 +0530

    setup clerk

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,22 @@
+import { ClerkProvider } from "@clerk/nextjs"
+import { Inter } from "next/font/google"
+import "../globals.css"
+
+export const metadata = {
+    title: 'Thread-X',
+    component: 'A Next.js 13 Thread-X Application',
+}
+
+const inter = Inter({subsets: ["latin"]})  
+
+export default function RootLayout ({ children }: { children: React.ReactNode }) {
+    return (
+        <ClerkProvider>
+            <html lang="en">
+                <body className={`${inter.className} bg-dark-1 `}>
+                    {children}
+                </body>
+            </html>
+        </ClerkProvider>
+    )
+}
