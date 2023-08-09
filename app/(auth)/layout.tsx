22bb00b982c7a161c4fe5dc63d73a648162192d4
crazyhaller <suvigyamadrid@gmail.com>
2023-08-09 15:49:07 +0530

    add activity page and com card and search bar comp

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,22 +1,28 @@
-import { ClerkProvider } from "@clerk/nextjs"
-import { Inter } from "next/font/google"
-import "../globals.css"
+import { ClerkProvider } from '@clerk/nextjs'
+import { Inter } from 'next/font/google'
+import '../globals.css'
 
 export const metadata = {
-    title: 'Thread-X',
-    component: 'A Next.js 13 Thread-X Application',
+  title: 'Thread-X',
+  component: 'A Next.js 13 Thread-X Application',
 }
 
-const inter = Inter({subsets: ["latin"]})  
+const inter = Inter({ subsets: ['latin'] })
 
-export default function RootLayout ({ children }: { children: React.ReactNode }) {
-    return (
-        <ClerkProvider>
-            <html lang="en">
-                <body className={`${inter.className} bg-dark-1 `}>
-                    {children}
-                </body>
-            </html>
-        </ClerkProvider>
-    )
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-1 `}>
+          <div className="w-full flex justify-center items-center min-h-screen">
+            {children}
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
+  )
 }
